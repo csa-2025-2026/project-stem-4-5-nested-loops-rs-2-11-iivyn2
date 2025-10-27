c46f69015da6f11812c9141e11f8ab60cd3dfa00
iivyn2 <idnguyen101@student.hbuhsd.edu>
2025-10-27 21:46:23 +0000

    problem 2 half done

--- Main.java
+++ Main.java
@@ -4,17 +4,35 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNTimes("hello",5);
   }
 
   public static void printNTimes(String word, int N)
   {
     // code solution here
-  }
+    for (int i = 0; i < word.length(); i++) // for each letter in the word... (loop)
+    {
+      String letter = word.substring(i, i+1);
+      for (int j = 0; j < N; j++)     // print that letter N times (loop)
+      {
+        System.out.print(letter);
+      }
+      System.out.println();
+      }
+    }
+
 
   public static void printNums()
   {
     // code solution here
+    for (int i = 10; i > 0; i--)
+    {
+      int N = 
+      for (int j = 0; j < 1; j++)
+      {
+        System.out.print(i);
+      }
+    }
   }
 
   public static void uprightNumberTriangle(int N)
@@ -31,4 +49,6 @@ public class Main
   {
     // code solution here
   }
+
+  
 }
